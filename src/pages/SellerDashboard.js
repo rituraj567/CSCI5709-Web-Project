9a6d6a024ae2f6b597d571e2d0fb2e7706dbd021
Nishith Vithaldas Gadhiya <ns421627@dal.ca>
2023-02-25 11:36:49 -0400

    modified folder structure

---
+++ src/pages/SellerDashboard.js
@@ -0,0 +1,12 @@
+import React from "react";
+import Navbar from "../components/SellerDashboard/Navbar";
+
+function SellerDashboard() {
+  return (
+    <div>
+      <Navbar />
+    </div>
+  );
+}
+
+export default SellerDashboard;

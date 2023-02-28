e4f2a0bc340467bcc5c33f1f9fd37bb71086903a
Nishith Vithaldas Gadhiya <ns421627@dal.ca>
2023-02-28 11:30:59 -0400

    lazyloading in product images

--- frontend/src/components/homePage/ProductsList.js
+++ frontend/src/components/homePage/ProductsList.js
@@ -5,19 +5,6 @@ import Grid from "@mui/material/Grid";
 
 let productsList = [
   {
-    id: 1,
-    title: "Fjallraven - Foldsack No. 1 Backpack, Fits 15 Laptops",
-    price: 109.95,
-    description:
-      "Your perfect pack for everyday use and walks in the forest. Stash your laptop (up to 15 inches) in the padded sleeve, your everyday",
-    category: "men's clothing",
-    image: "https://fakestoreapi.com/img/81fPKd-2AYL._AC_SL1500_.jpg",
-    rating: {
-      rate: 3.9,
-      count: 120,
-    },
-  },
-  {
     id: 2,
     title: "Mens Casual Premium Slim Fit T-Shirts ",
     price: 22.3,
@@ -189,19 +176,6 @@ let productsList = [
     rating: {
       rate: 2.2,
       count: 140,
-    },
-  },
-  {
-    id: 15,
-    title: "BIYLACLESEN Women's 3-in-1 Snowboard Jacket Winter Coats",
-    price: 56.99,
-    description:
-      "Note:The Jackets is US standard size, Please choose size as your usual wear Material: 100% Polyester; Detachable Liner Fabric: Warm Fleece. Detachable Functional Liner: Skin Friendly, Lightweigt and Warm.Stand Collar Liner jacket, keep you warm in cold weather. Zippered Pockets: 2 Zippered Hand Pockets, 2 Zippered Pockets on Chest (enough to keep cards or keys)and 1 Hidden Pocket Inside.Zippered Hand Pockets and Hidden Pocket keep your things secure. Humanized Design: Adjustable and Detachable Hood and Adjustable cuff to prevent the wind and water,for a comfortable fit. 3 in 1 Detachable Design provide more convenience, you can separate the coat and inner as needed, or wear it together. It is suitable for different season and help you adapt to different climates",
-    category: "women's clothing",
-    image: "https://fakestoreapi.com/img/51Y5NI-I5jL._AC_UX679_.jpg",
-    rating: {
-      rate: 2.6,
-      count: 235,
     },
   },
   {

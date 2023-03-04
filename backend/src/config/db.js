900a4c8973ba40274594840fbe39628cebc392b9
Nishith Vithaldas Gadhiya <ns421627@dal.ca>
2023-03-04 13:46:20 -0400

    added backend folder structure

---
+++ backend/src/config/db.js
@@ -0,0 +1,7 @@
+require("dotenv").config();
+const mongoose = require("mongoose");
+
+// mongoose
+//   .connect(process.env.MONGODB_URL)
+//   .then(() => console.log("DB Connected"))
+//   .catch((err) => console.log(err));

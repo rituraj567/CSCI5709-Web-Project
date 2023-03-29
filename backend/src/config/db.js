8c4525b28374fcb8b9f77cf782be07bc1ae0db8c
Rituraj Kadamati <rt431563@dal.ca>
2023-03-29 17:32:16 -0300

    made changes in db

--- backend/src/config/db.js
+++ backend/src/config/db.js
@@ -1,20 +1,17 @@
-const mongoose = require('mongoose');
-require('dotenv').config();
+const mongoose = require("mongoose");
+require("dotenv").config();
 
 const uri = process.env.MONGODB_URI;
-const Rating = require('../features/rating/model');
-const {Product,saveProducts} = require('../features/product/model');
-mongoose.connect("mongodb+srv://Group-10:r7VCsxNRGOrqQNT5@cluster0.vsbpsvc.mongodb.net/?retryWrites=true&w=majority", { useNewUrlParser: true, useUnifiedTopology: true })
+const Rating = require("../features/rating/model");
+const { Product, saveProducts } = require("../features/product/model");
+mongoose
+  .connect(
+    "mongodb+srv://Group-10:r7VCsxNRGOrqQNT5@cluster0.vsbpsvc.mongodb.net/?retryWrites=true&w=majority",
+    { useNewUrlParser: true, useUnifiedTopology: true }
+  )
   .then(() => {
     console.log("Connected to MongoDB Atlas successfully");
   })
   .catch((err) => {
     console.error("Error connecting to MongoDB Atlas: ", err);
   });
-
-  
-
-
-
-
-

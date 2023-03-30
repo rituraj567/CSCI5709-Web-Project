dba0072701ed6c1936faf1b3c037524f60293de7
tarora <tarora>
2023-03-30 15:08:42 -0300

    changes in index file

--- backend/src/routes/index.js
+++ backend/src/routes/index.js
@@ -5,7 +5,7 @@ const router = express.Router();
 const userRoutes = require("../features/userManagement");
 const testRoutes = require("./../features/testing");
 const accountRoutes=require("./../features/accountManagement");
-
+const productRoutes=require("../features/product");
 
 router.use("/test", testRoutes);
 router.use("/user", userRoutes);
